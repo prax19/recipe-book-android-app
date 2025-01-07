dcf4458785c5cc9aae19028ba07d415e185612f2
prax19 <fhcarpaty2@gmail.com>
2025-01-07 21:49:15 +0100

    Add basic MainView layout

--- app/src/main/java/pl/prax19/recipe_book_app/presentation/MainView.kt
+++ app/src/main/java/pl/prax19/recipe_book_app/presentation/MainView.kt
@@ -1,14 +1,52 @@
 package pl.prax19.recipe_book_app.presentation
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainView() {
-    Scaffold {
-        Text("test")
-    }
+
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+
+    Scaffold (
+        topBar = {
+            MediumTopAppBar(
+                title = { Text("Recipe Book") },
+                scrollBehavior = scrollBehavior
+            )
+        },
+        content = {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .nestedScroll(scrollBehavior.nestedScrollConnection),
+                contentPadding = it,
+                verticalArrangement = Arrangement.spacedBy(4.dp),
+                content = {
+                    items(100) {
+                        // TODO: add recipe object representation
+                        Text(
+                            text = "Test recipe ${it}",
+                            modifier = Modifier.padding(horizontal = 16.dp)
+                        )
+                    }
+                }
+            )
+        }
+    )
 }
